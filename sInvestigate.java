4db7cee387be668854cd80ef41e92b37cbbee874
YellowSubject <1319660949@qq.com>
2020-01-04 14:19:13 +0800

    entity

---
+++ sInvestigate.java
@@ -0,0 +1,76 @@
+package sInvestigateEntity;
+
+public class sInvestigate {
+private int id;
+private String option;
+private String name;
+private String time;
+private String violatFlag;
+private String violateCesc;
+private String remark;
+private String appNo;
+private String prioCode;
+private String keyconsFile;
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getOption() {
+	return option;
+}
+public void setOption(String option) {
+	this.option = option;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getTime() {
+	return time;
+}
+public void setTime(String time) {
+	this.time = time;
+}
+public String getViolatFlag() {
+	return violatFlag;
+}
+public void setViolatFlag(String violatFlag) {
+	this.violatFlag = violatFlag;
+}
+public String getViolateCesc() {
+	return violateCesc;
+}
+public void setViolateCesc(String violateCesc) {
+	this.violateCesc = violateCesc;
+}
+public String getRemark() {
+	return remark;
+}
+public void setRemark(String remark) {
+	this.remark = remark;
+}
+public String getAppNo() {
+	return appNo;
+}
+public void setAppNo(String appNo) {
+	this.appNo = appNo;
+}
+public String getPrioCode() {
+	return prioCode;
+}
+public void setPrioCode(String prioCode) {
+	this.prioCode = prioCode;
+}
+public String getKeyconsFile() {
+	return keyconsFile;
+}
+public void setKeyconsFile(String keyconsFile) {
+	this.keyconsFile = keyconsFile;
+}
+
+
+}
